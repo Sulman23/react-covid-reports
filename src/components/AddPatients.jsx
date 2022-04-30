469f7dbc06d67d8ec430550f19aa71f307a5fe11
Suleman Ahmad <sulmq23@gmail.com>
2022-04-30 00:53:32 -0700

    working on Add Patient form

---
+++ src/components/AddPatients.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const AddPatients = () => {
+  return (
+    <div>
+        <form>
+            <div className="my-3">
+                <label for="name" className="form-label">Full Name</label>
+                <input type="text" className="form-control" id="name" aria-describedby="emailHelp" placeholder="Jhon Doe" />
+            </div>   
+            <div className="mb-3">
+                <label for="email" className="form-label">Email</label>
+                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="email@email.com"/>
+            </div>   
+            <div className="mb-3">
+                <label for="phone" className="form-label">Phone Number</label>
+                <input type="number" className="form-control" id="phone" aria-describedby="emailHelp" placeholder="0123456789"/>
+            </div> 
+            <div class="mb-3">
+                <label for="address" class="form-label">Address</label>
+                <textarea class="form-control" id="address" rows="3"></textarea>
+            </div>  
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default AddPatients
