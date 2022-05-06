b26069c5527ee1cc833b24cead3f67365e3c4d20
Suleman Ahmad <sulmq23@gmail.com>
2022-05-06 00:49:14 -0700

    working on home

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
       {/* <AddPatients />  */}
-      {/* <Login /> */}
+      <Login />
       {/* <Home /> */}
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <Footer /> */}
     </div>
   );
